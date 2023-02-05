3f18886552bbd66db9091f1c37733de55715c121
Shiva Eslami <shivaslami@gmail.com>
2023-02-06 01:21:25 +0330

    Updated responsive style

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,7 +4,7 @@ import TimeAndLocation from "./TimeAndLocation";
 
 const Result = ({ data }) => {
   return (
-    <section className="container flex justify-around items-start px-7 mx-auto h-fit py-5 lg:h-screen">
+    <section className="container flex flex-col justify-center items-center px-7 mx-auto h-fit py-5 lg:justify-around lg:items-start lg:flex-row lg:h-screen">
       <Temperature data={data} />
       <TimeAndLocation data={data} />
     </section>
