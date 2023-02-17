4484988d7fa6d836b5678255e4fc5464f9db1e78
Shiva Eslami <shivaslami@gmail.com>
2023-02-17 23:55:42 +0330

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Search from "./Search";
-import TopButtons from "./CityButtons";
+import CityButtons from "./CityButtons";
 import WeatherDetails from "./WeatherDetails";
 
 const Sidebar = ({ data, onCityChange, units, setUnits }) => {
   return (
     <div className="sidebar pl-12">
       <Search units={units} setUnits={setUnits} onCityChange={onCityChange} />
-      <TopButtons onCityChange={onCityChange} />
+      <CityButtons onCityChange={onCityChange} />
       <div
         className="border-solid 
         border-b-[1px] border-b-slate-400 w-[92%] my-9"
